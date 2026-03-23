de69eda3208f48d3aa948a15700e7396eca63c66
flareon30-byte <flareon30@gmail.com>
2026-03-23 19:33:02 +0100

    fix: Remove unused piexifjs dependency causing container crash loop

--- server/src/controllers/simpleInstallationController.js
+++ server/src/controllers/simpleInstallationController.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const piexif = require("piexifjs");
 
 exports.createInstallation = async (req, res) => {
     try {
